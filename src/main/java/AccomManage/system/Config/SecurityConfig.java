56727438324d0f948b2c7179fb8a46d65674c82c
Chean Long Phan <cheanlongphan@gmail.com>
2026-07-20 17:24:48 +0700

    add link

--- src/main/java/AccomManage/system/Config/SecurityConfig.java
+++ src/main/java/AccomManage/system/Config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://anachak-devs.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
